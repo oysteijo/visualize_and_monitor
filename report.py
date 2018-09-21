38e39a84eed76fcfdc04d22452da2d269a898263
Oystein Schonning-Johansen <oysteijo@gmail.com>
2018-09-21 23:51:07 +0200

    Add the basic things in the reporter

--- report.py
+++ report.py
@@ -16,50 +16,61 @@ class Reporter(object):
                         })
     """
 
-    def __init__(self, database=None, fields=None):
+    def __init__(self, database=None):
         if not database:
             database = "reporter.db"
 
         self.database   = database
         self.connection = sqlite3.connect(self.database)
         self.cursor     = self.connection.cursor()
-        if fields:
-            self.set_fields(fields)
+        self.table_created = self._check_table()
 
-    def set_fields(self, fields):
-        integer_indicators = ["epoch", "step", "fold"]
+    def _check_table(self):
+        try:
+            self.cursor.execute("""
+                SELECT COUNT(*)
+                FROM information_schema.tables
+                WHERE table_name = '{0}'
+                """.format(TABLENAME.replace('\'', '\'\'')))
+            return self.cursor.fetchone()[0] == 1
+        except:
+            return False
 
-        int_fields = set() 
-        for f in fields:
-            if any([ii in f for ii in integer_indicators]):
-                int_fields.add(f)
-
-        #print(int_fields)
-
-        float_fields = set(fields) - int_fields
-        #print(float_fields)
-
+    def _create_table(self, values):
+        d = { str: "TEXT", float: "FLOAT", int: "INTEGER" }
         create_str = """create table if not exists {}(
-            id INTEGER PRIMARY KEY,
-            date DATETIME,
-            description TEXT,
-            {})""".format( TABLENAME, ", ".join([ f+ " INTEGER\n" for f in int_fields] ))
-        print(create_str)
+            id INTEGER PRIMARY KEY
+            ,d1 INT  -- Date Stored in unixtime (seconds since 1st.Jan.1970) 
+            """.format(TABLENAME)
+        for k,v in values.items():
+            create_str += "\n      ,{} {}".format(str(k), d[type(v)] )
+        create_str += ")"
 
-
-        # self.cursor.execute("""create table if not exists training_log(
+        #print( create_str )
+        self.cursor.execute( create_str )
+        self.connection.commit()
+        self.table_created = True
 
     def write(self, values ):
+        if not self.table_created:
+            print("No table found?")
+            self._create_table(values)
+
         columns = ", ".join(values.keys())
         #columns += ", date"
         placeholders = ":"+", :".join(values.keys())
         #placeholders += ", :", datetime.now()
-        query = 'INSERT INTO %s (%s) VALUES (%s)' % (TABLENAME, columns, placeholders)
-        print( query )
-        #cur.execute(query, values)
-        #con.commit()
+        query = """INSERT INTO {} (d1, {}) VALUES (strftime('%s', 'now'), {})""".format(TABLENAME, columns, placeholders)
+        #print( query )
+        self.cursor.execute(query, values)
+        self.connection.commit()
 
 if __name__ == "__main__":
-    reporter = Reporter(fields=["epoch", "score", "steps", "bigsteps", "foldepoch"])
-    reporter.write({"epoch": 2, "score": 4.0})
+    reporter = Reporter()
+    reporter.write({
+        "description": "new loss function",
+        "epoch"      : 2,
+        "score"      : 4.0
+    })
+
 
